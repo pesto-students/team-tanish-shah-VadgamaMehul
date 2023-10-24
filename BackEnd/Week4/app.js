c3c7515cb6378425043ab4b2fbadc7c80b0ab0a6
Mehul Vadgama <vadgama.mehul444@gmail.com>
2023-10-24 23:54:26 +0530

    Add NJ Week 4 Assignment

---
+++ BackEnd/Week4/app.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const readline = require("readline");
+const generateGreeting = require("./greetings");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What is your name? ", (name) => {
+  const greeting = generateGreeting(name);
+
+  fs.writeFile("output.txt", greeting, (err) => {
+    if (err) {
+      console.error("Error :", err);
+    } else {
+      console.log("Greeting saved to output.txt.");
+    }
+    rl.close();
+  });
+});
